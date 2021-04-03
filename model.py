9220abf85f295045f7801ae7259752afc0d144df
ferolifil <ferolifil@gmail.com>
2021-04-03 00:46:50 -0300

    model creation

--- model.py
+++ model.py
@@ -23,148 +23,148 @@ def boxplot(column):
     ax2.set_xlim(limit_calc(column))
     sns.boxplot(x=column, ax=ax2)
     
-def histogram(column,color=None):
-    plt.figure(figsize=(15, 5))
-    sns.histplot(column,color=color)
+# def histogram(column,color=None):
+#     plt.figure(figsize=(15, 5))
+#     sns.histplot(column,color=color)
 
 def bar_chart(column):  
     plt.figure(figsize=(15, 5))
     ax = sns.barplot(x=column.value_counts().index, y=column.value_counts())
     ax.set_xlim(limit_calc(column))
 
-def model_score(dataframe,full_nba_dataset,model,features,iterations=1,path='./misses/trash'):
+# def model_score(dataframe,full_nba_dataset,model,features,iterations=1,path='./misses/trash'):
     
-    pbar = tqdm(total = 40)
+#     pbar = tqdm(total = 40)
     
-    features.remove('Share')
-    features.remove('Season')
+#     features.remove('Share')
+#     features.remove('Season')
     
-    nro_of_corrects = 0
-    nro_of_top3_corrects = 0
+#     nro_of_corrects = 0
+#     nro_of_top3_corrects = 0
 
-    file = open(f"{path}/output.txt","w")
+#     file = open(f"{path}/output.txt","w")
     
-    file.write('Model : {}\n{} features : {}\n'.format(model,len(features),features))
+#     file.write('Model : {}\n{} features : {}\n'.format(model,len(features),features))
 
-    t1 = time.time()
+#     t1 = time.time()
 
-    for season in np.arange(1981,2021,1):
+#     for season in np.arange(1981,2021,1):
 
-        text = f' {season} |'
+#         text = f' {season} |'
 
-        y_train = dataframe.loc[(dataframe['Season'] != season),['Share']]
-        y_train = np.ravel(y_train)
-        y_test = dataframe.loc[(dataframe['Season'] == season),['Share']]
-        y_test = np.ravel(y_test)
+#         y_train = dataframe.loc[(dataframe['Season'] != season),['Share']]
+#         y_train = np.ravel(y_train)
+#         y_test = dataframe.loc[(dataframe['Season'] == season),['Share']]
+#         y_test = np.ravel(y_test)
 
-        X_train = dataframe.loc[(dataframe['Season'] != season),:].drop(['Share','Season'], axis=1)
-        X_test = dataframe.loc[(dataframe['Season'] == season),:].drop(['Share','Season'], axis=1)
+#         X_train = dataframe.loc[(dataframe['Season'] != season),:].drop(['Share','Season'], axis=1)
+#         X_test = dataframe.loc[(dataframe['Season'] == season),:].drop(['Share','Season'], axis=1)
 
-        players_rank = dict()
-        df = full_nba_dataset.loc[(full_nba_dataset['Season'] == season),['Player','Share','Season']]
-        df_truth = df.sort_values(by='Share', ascending=False)
+#         players_rank = dict()
+#         df = full_nba_dataset.loc[(full_nba_dataset['Season'] == season),['Player','Share','Season']]
+#         df_truth = df.sort_values(by='Share', ascending=False)
 
-        for i in range(iterations):
+#         for i in range(iterations):
 
-            model.fit(X_train, y_train)
-            pred = model.predict(X_test)
+#             model.fit(X_train, y_train)
+#             pred = model.predict(X_test)
 
-            df['Pred'] = pred.tolist()
-            df_pred = df.drop(['Share'],axis=1).sort_values(by='Pred', ascending=False)
+#             df['Pred'] = pred.tolist()
+#             df_pred = df.drop(['Share'],axis=1).sort_values(by='Pred', ascending=False)
 
-            for j in range(5):
-                if df_pred.iat[j,0] in players_rank.keys():
-                    players_rank[df_pred.iat[j,0]] += (10-(j*2))
-                else:
-                    players_rank[df_pred.iat[j,0]] = (10-(j*2))
+#             for j in range(5):
+#                 if df_pred.iat[j,0] in players_rank.keys():
+#                     players_rank[df_pred.iat[j,0]] += (10-(j*2))
+#                 else:
+#                     players_rank[df_pred.iat[j,0]] = (10-(j*2))
 
-        players_columns = list(players_rank.keys())
-        votes_columns = []
+#         players_columns = list(players_rank.keys())
+#         votes_columns = []
 
-        for k in players_columns:
-            votes_columns.append(players_rank[k])
+#         for k in players_columns:
+#             votes_columns.append(players_rank[k])
 
-        data = dict()
-        data['Player'] = players_columns
-        data['Season'] = season
-        data['Pred'] = votes_columns
+#         data = dict()
+#         data['Player'] = players_columns
+#         data['Season'] = season
+#         data['Pred'] = votes_columns
 
-        df_pred_final = pd.DataFrame(data)
-        df_pred_final = df_pred_final.sort_values(by='Pred', ascending=False)
-        df_pred_final.to_csv(f"./predictions/{season}_pred.csv")
+#         df_pred_final = pd.DataFrame(data)
+#         df_pred_final = df_pred_final.sort_values(by='Pred', ascending=False)
+#         df_pred_final.to_csv(f"./predictions/{season}_pred.csv")
 
-        pred_top3 = list(df_pred_final['Player'].iloc[:3])
-        top3 = list(df_truth['Player'].iloc[:3]) 
-        top3_success = list()
+#         pred_top3 = list(df_pred_final['Player'].iloc[:3])
+#         top3 = list(df_truth['Player'].iloc[:3]) 
+#         top3_success = list()
 
-        for z in pred_top3:
-            if z in top3:
-                top3_success.append(z)
+#         for z in pred_top3:
+#             if z in top3:
+#                 top3_success.append(z)
 
-        df_comp = pd.DataFrame()
+#         df_comp = pd.DataFrame()
 
-        if pred_top3[0] == top3[0]:
-            sorf = 'Success'
-            nro_of_corrects += 1
-        else:
-            sorf = 'Fail'
-            df_comp = full_nba_dataset.loc[(full_nba_dataset['Season'] == season) & (full_nba_dataset['Player'] == top3[0]) ,:]
-            df_comp = df_comp.append(full_nba_dataset.loc[(full_nba_dataset['Season'] == season) & (full_nba_dataset['Player'] == pred_top3[0]) ,:])
-            df_comp['Label'] = ['Correct','Predicted']
-            df_comp.to_csv('{}/{}_miss.csv'.format(path,season),index=False)
+#         if pred_top3[0] == top3[0]:
+#             sorf = 'Success'
+#             nro_of_corrects += 1
+#         else:
+#             sorf = 'Fail'
+#             df_comp = full_nba_dataset.loc[(full_nba_dataset['Season'] == season) & (full_nba_dataset['Player'] == top3[0]) ,:]
+#             df_comp = df_comp.append(full_nba_dataset.loc[(full_nba_dataset['Season'] == season) & (full_nba_dataset['Player'] == pred_top3[0]) ,:])
+#             df_comp['Label'] = ['Correct','Predicted']
+#             df_comp.to_csv('{}/{}_miss.csv'.format(path,season),index=False)
 
-        text += '{:^10s}| '.format(sorf)
+#         text += '{:^10s}| '.format(sorf)
 
 
-        text += f'{len(top3_success)} from de top3 correct | {top3_success} |\n'
-        nro_of_top3_corrects += len(top3_success)
+#         text += f'{len(top3_success)} from de top3 correct | {top3_success} |\n'
+#         nro_of_top3_corrects += len(top3_success)
 
-        file.write(str(text))
+#         file.write(str(text))
         
-        pbar.update()
+#         pbar.update()
 
-    text = "GLOBAL_SUCCESS_RATE : {:.2%}\nGLOBAL_SUCCESS_RATE_TOP3 : {:.2%}\n".format((nro_of_corrects/40),(nro_of_top3_corrects/120))
-    file.write(str(text))
-    t2 = time.time()
+#     text = "GLOBAL_SUCCESS_RATE : {:.2%}\nGLOBAL_SUCCESS_RATE_TOP3 : {:.2%}\n".format((nro_of_corrects/40),(nro_of_top3_corrects/120))
+#     file.write(str(text))
+#     t2 = time.time()
 
-    text = 'Time elapsed: {:.2f} seconds'.format(t2-t1)
-    file.write(str(text))
-    file.close()
+#     text = 'Time elapsed: {:.2f} seconds'.format(t2-t1)
+#     file.write(str(text))
+#     file.close()
     
-    return (nro_of_corrects/40),(nro_of_top3_corrects/120)
+#     return (nro_of_corrects/40),(nro_of_top3_corrects/120)
 
-def feature_selection(dataframe, model):
+# def feature_selection(dataframe, model):
     
-    if 'Season' in dataframe.columns:
-        dataframe = dataframe.drop(columns='Season')
+#     if 'Season' in dataframe.columns:
+#         dataframe = dataframe.drop(columns='Season')
         
-    try:
-        X = dataframe.drop(['Share'], axis=1)
-        y = dataframe['Share']
-    except:
-        print('Share feature must be in')
+#     try:
+#         X = dataframe.drop(['Share'], axis=1)
+#         y = dataframe['Share']
+#     except:
+#         print('Share feature must be in')
         
-    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size = 0.8, random_state=10)
+#     X_train, X_test, y_train, y_test = train_test_split(X, y, train_size = 0.8, random_state=10)
     
-    model.fit(X_train, y_train)
+#     model.fit(X_train, y_train)
 
-    data_dict = {'Features' : X_train.columns, 'Importance' : model.feature_importances_}
-    features = pd.DataFrame(data_dict).sort_values(by='Importance',ascending=False)
-    features = features.loc[(features['Importance'] > 0) , :]
-    feature_list = features['Features'].to_list()
+#     data_dict = {'Features' : X_train.columns, 'Importance' : model.feature_importances_}
+#     features = pd.DataFrame(data_dict).sort_values(by='Importance',ascending=False)
+#     features = features.loc[(features['Importance'] > 0) , :]
+#     feature_list = features['Features'].to_list()
 
-    return feature_list
+#     return feature_list
 
-def test_model(dataframe,model,iterations,path='./misses/trash'):
+# def test_model(dataframe,model,iterations,path='./misses/trash'):
     
-    feature_list = feature_selection(dataframe=dataframe,model=model)
-    feature_list.extend(['Share', 'Season'])
+#     feature_list = feature_selection(dataframe=dataframe,model=model)
+#     feature_list.extend(['Share', 'Season'])
 
-    df = full_nba_dataset[feature_list]
+#     df = full_nba_dataset[feature_list]
 
-    nro_of_corrects, nro_of_top3_corrects = model_score(dataframe=dataframe,model=model,features=feature_list,iterations=iterations,path=path)
+#     nro_of_corrects, nro_of_top3_corrects = model_score(dataframe=dataframe,model=model,features=feature_list,iterations=iterations,path=path)
 
-    return nro_of_corrects, nro_of_top3_corrects
+#     return nro_of_corrects, nro_of_top3_corrects
 
 
 def main():
