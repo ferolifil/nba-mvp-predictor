5de6865119f66a49331b45a8c539862ea7157ea1
ferolifil <ferolifil@gmail.com>
2021-04-05 17:03:31 -0300

    streamlit first steps

---
+++ .ipynb_checkpoints/testpy-checkpoint.py
@@ -0,0 +1,12 @@
+from scrap import scrap, scrap_teams, get_teams, scrap_latest
+from std import adjust_main, adjust_mvp, transfered_players, merging_df, teams_std, std_latest
+import pandas as pd
+
+season = 2021
+
+scrap_latest(season)
+nba2021 = std_latest(season)
+
+                                    # RUNNING THE ANALYSIS #
+######################################################################################################
+
