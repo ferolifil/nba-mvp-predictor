7a759eedbd1258f8a06b3af7ea4688d2477a03b6
ferolifil <ferolifil@gmail.com>
2021-04-07 20:41:31 -0300

    pred latest func

--- model.py
+++ model.py
@@ -1,14 +1,6 @@
 import pandas as pd
 import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
-import plotly.express as px
-import time
 from sklearn.ensemble import RandomForestRegressor, ExtraTreesRegressor
-from sklearn import tree
-from sklearn.model_selection import train_test_split
-from sklearn import tree
-from tqdm import tqdm
 
 def limit_calc(column):
     q1 = column.quantile(0.25)
@@ -16,7 +8,7 @@ def limit_calc(column):
     amplitude = q3 - q1
     return q1 - 1.5 * amplitude, q3 + 1.5 * amplitude
 
-def main():
+def pred_latest():
 
     nba_dataset = pd.DataFrame()
 
@@ -24,7 +16,7 @@ def main():
         df = pd.read_csv(f'./data/{season}_std.csv', low_memory=False)    
         nba_dataset = nba_dataset.append(df)
 
-    cols = ['MP','FG','FG%','3P','2P','FT','TRB','DRB%','ORB%','OWS','DWS','WS/48','OBPM','DBPM','First']
+    cols = ['MP','FG','FG%','3P','2P','FT','DRB%','ORB%','OWS','DWS','WS/48','OBPM','DBPM','First']
     nba_dataset = nba_dataset.drop(columns=cols)
 
     nba_dataset = nba_dataset.loc[((nba_dataset['3P%'] >= 0.0) & (nba_dataset['3P%'] <= 0.6)) 
@@ -107,25 +99,37 @@ def main():
     df_pred_final = df_pred_final.sort_values(by='Pred', ascending=False)
     df_pred_final.to_csv(f"./last_prediction.csv")
 
-
     data2 = dict()
     data2['Player'] = players_first_list
     data2['First'] = votes_first
 
     df_temp = pd.DataFrame(data2)
     df_temp2 = df_pred_final.join(df_temp.set_index('Player'), on='Player')
-    df_temp2 = df_temp2.fillna(0)
-    
-    print(df_temp2)
+    df_temp2 = df_temp2.fillna(0.0)
+    df_temp2 = df_temp2[:5] 
+    df_temp2 = df_temp2.astype({'Pred': 'float32', 'First': 'float32'})
 
-    # df1 = df1.join(df2.set_index(keys), on=keys)
+    sum_share = df_temp2['Pred'].sum(axis = 0, skipna = False)
+    sum_first = df_temp2['First'].sum(axis = 0, skipna = False)
 
-    # df_pred_final = df_pred_final[:5]
+    pct_list = []
 
-    # for i in df_pred_final.index:
-    #     print(i) 
+    for i in df_temp2.index:
+        value = (df_temp2.at[i,'Pred'] + df_temp2.at[i,'First'])/(sum_share + sum_first)
+        pct_list.append("{:.2%}".format(value))
 
+    df_temp2['Prob%'] = pct_list
+    df_temp2 = df_temp2.drop(['Pred','First'],axis=1)   
+    df_temp2['Rk'] = [1,2,3,4,5]
+    df_top5 = df_temp2.set_index('Rk') 
 
+    mvp = df_temp2.iat[0,0]
+    mvp_data = full_nba_dataset.loc[(full_nba_dataset['Player'] == mvp) & (full_nba_dataset['Season'] == season), ['Player','Pos','Age','Tm','PTS','TRB','AST','BLK','STL','TOV','PF','G']]
+    mvp_data = mvp_data.astype({'PTS' : 'float32', 'TRB' : 'float32', 'AST' : 'float32', 'BLK' : 'float32', 'STL' : 'float32', 'TOV' : 'float32', 'PF' : 'float32' })
 
-if __name__ == "__main__":
-    main()
+    for i in np.arange(4,11,1):
+        mvp_data.iat[0,i] = "{:.2f}".format(mvp_data.iat[0,i] / mvp_data.iat[0,11]) 
+
+    mvp_data = mvp_data.drop(columns='G')
+
+    return df_top5, mvp_data
