26d1a5cbf43ccf8942e08d8fb603b5779760154c
ferolifil <ferolifil@gmail.com>
2021-04-09 21:16:40 -0300

    last update

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
 import streamlit as st
 import time
-from scrap import scrap_latest
-from std import std_latest
-from model import limit_calc, pred_latest
+from lib.scrap import scrap_latest
+from lib.std import std_latest
+from lib.model import limit_calc, pred_latest
 import pandas as pd
 import numpy as np
 from pyngrok import ngrok
 
 def main():
+
+  img_dict = {
+    'Nikola Jokić' : 'nikola.jpg', 
+    'James Harden' : 'harden.jpg', 
+    'Joel Embiid' : 'joel.jpg', 
+    'Damian Lillard' : 'dame.jpg' , 
+    'Kawhi Leonard' : 'kawhi.jpg', 
+    'Giannis Antetokounmpo' : 'giannis.jpg', 
+    'Kevin Durant' : 'kd.jpg', 
+    'Kyrie Irving' : 'kyrie.jpg', 
+    'LeBron James' : 'lbj.jpg', 
+    'Luka Dončić' : 'luka.jpg', 
+    'Rudy Gobert' : 'rudy.jpg'  }
 
   season = 2021
 
@@ -20,20 +33,20 @@ def main():
 
     scrap_latest(season)
     nba2021 = std_latest(season)
-    df_top5, mvp_stats = pred_latest()     
+    df_top5, mvp_stats = pred_latest()   
+
+    mvp = mvp_stats.iat[0,0]  
+    print(mvp)
 
     st.table(df_top5)
 
     mvp_stats = mvp_stats.set_index('Player')
     st.table(mvp_stats.style.format({"PTS" : "{:.2f}", 'TRB' : "{:.2f}", 'AST' : "{:.2f}", 'BLK' : "{:.2f}", 'STL' : "{:.2f}", 'FG%' : "{:.2%}", 'FT%' : "{:.2%}"}))
 
+    st.image('./img/{}'.format(img_dict[mvp]))
 
-    st.image('./nikola.jpg')
   try:
     public_url = ngrok.connect('8501')
-
-    # public_url
-    # http://72eec084c813.ngrok.io
   except:
     pass
 
